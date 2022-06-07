7c86000bf5c456170edbd0f3c6b5ea2fb987a3fd
BehemothDi <dmitriiprozherin@mail.ru>
2022-06-08 02:13:39 +0300

    Design preview v0.3.3

--- frontend/views/site/gallery.php
+++ frontend/views/site/gallery.php
@@ -4,17 +4,16 @@
 
 use yii\helpers\Html;
 
-$this->title = 'Галлерея';
+$this->title = 'Музейно-выставочный центр «Галерея безопасности Мурманской области»';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <section class="jumbotron text-center">
-        <div class="container">
-            <h1 class="jumbotron-heading">Album example</h1>
-            <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-            <p> <a href="#" class="btn btn-primary my-2">Main call to action</a> <a href="#" class="btn btn-secondary my-2">Secondary action</a> </p>
+    <section class=" text-center">
+        <iframe src="https://www.mospano.ru/museums/2020/murmansk/" width="100%"  height="600" style="border:0; border-radius: 7px; overflow: hidden" allowfullscreen></iframe>
+        <div class="bg-dark container-fluid text-white">
+            <a href="https://www.mospano.ru/museums/2020/murmansk/" target="_blank">Открыть виртуальный тур в новом окне</a>
         </div>
     </section>
 
