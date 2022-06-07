548195c506d236a4a067818e162b68e7ab3643e5
BehemothDi <dmitriiprozherin@mail.ru>
2022-06-07 23:54:09 +0300

    Design preview v0.1

--- frontend/views/site/news.php
+++ frontend/views/site/news.php
@@ -13,62 +13,40 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-<!--    --><?php
+    <?php
+
+    $news_list = News::find()->asArray()->all();
+
+
+//    for ($i = 1; $i <= 5; $i++) {
+//        print_r($news_list);
+//    }
 //
-//    $news = News::find()->asArray()->all();
 //
-//    print_r($news);
+//
+//
 //
 //    ?>
 
     <div class="album py-5 bg-light">
         <div class="container">
-    <div class="col-lg-12 col-md-6">
+
+            <?php foreach ($news_list as $news):?>
+
+
+
+            <div class="col-lg-12 col-md-6">
         <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <img class="card-img-right flex-auto d-none d-md-block" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1610ecb1e61%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1610ecb1e61%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-            <div class="card-body d-flex flex-column align-items-start"><strong class="d-inline-block mb-2 text-success">Design</strong>
-                <h3 class="mb-0"> <a class="text-dark" href="#">Post title</a> </h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p><a href="#">Continue reading</a>
+            <img class="card-img-right flex-auto d-none d-md-block" src="<?php echo $news['image_link'];?>" alt="Card image cap" width="300" height="300"/>
+            <div class="card-body d-flex flex-column align-items-start"><strong class="d-inline-block mb-2 text-success"><?php echo $news['author'];?></strong>
+                <h3 class="mb-0"> <a class="text-dark" href="#"><?php echo $news['title'];?></a> </h3>
+                <div class="mb-1 text-muted"><?php echo $news['datetime'];?></div>
+                <p class="card-text mb-auto"><?php echo $news['short_desc'];?></p><a href="#">Посмотреть подробнее</a>
             </div>
         </div>
     </div>
+            <?php endforeach;?>
 
-    <div class="col-lg-12 col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <img class="card-img-right flex-auto d-none d-md-block" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1610ecb1e61%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1610ecb1e61%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-            <div class="card-body d-flex flex-column align-items-start"><strong class="d-inline-block mb-2 text-success">Design</strong>
-                <h3 class="mb-0"> <a class="text-dark" href="#">Post title</a> </h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p><a href="#">Continue reading</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-12 col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <img class="card-img-right flex-auto d-none d-md-block" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1610ecb1e61%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1610ecb1e61%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-            <div class="card-body d-flex flex-column align-items-start"><strong class="d-inline-block mb-2 text-success">Design</strong>
-                <h3 class="mb-0"> <a class="text-dark" href="#">Post title</a> </h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p><a href="#">Continue reading</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-12 col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <img class="card-img-right flex-auto d-none d-md-block" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1610ecb1e61%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1610ecb1e61%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-            <div class="card-body d-flex flex-column align-items-start"><strong class="d-inline-block mb-2 text-success">Design</strong>
-                <h3 class="mb-0"> <a class="text-dark" href="#">Post title</a> </h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p><a href="#">Continue reading</a>
-            </div>
-        </div>
-
-
-
-    </div>
 
     <div class="container text-left">
                 <ul class="d-flex flex-row justify-content-center pagination text-left">
