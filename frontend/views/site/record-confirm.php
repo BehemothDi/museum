af851b0bd9df3a9eef231ac366f403a9b73d443b
BehemothDi <dmitriiprozherin@mail.ru>
2022-06-30 23:22:50 +0300

    database update

--- frontend/views/site/record-confirm.php
+++ frontend/views/site/record-confirm.php
@@ -1,20 +1,33 @@
 <?php
+
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Record */
 /* @var $form ActiveForm */
-?>
-<p>Ваша заявка принята</p>
 
-<ul>
-    <li><label>Name</label>: <?= Html::encode($model->name) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->surname) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->patronymic) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->phone) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->email) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->date) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->amount_info) ?></li>
-    <li><label>Email</label>: <?= Html::encode($model->commentary) ?></li>
-</ul>
+
+$this->title = 'О нас';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="site-about">
+    <div class="pl-5 pr-5 pt-2 pb-5 bg-light">
+        <div class="record w-50">
+            <p class="bg-success w-50 p-2">Ваша заявка принята</p>
+
+            <ul>
+                <li><label>Имя</label>: <?= Html::encode($model->name) ?></li>
+                <li><label>Фамилия</label>: <?= Html::encode($model->surname) ?></li>
+                <li><label>Отчество</label>: <?= Html::encode($model->patronymic) ?></li>
+                <li><label>Электронная почта</label>: <?= Html::encode($model->email) ?></li>
+                <li><label>Телефон</label>: <?= Html::encode($model->phone) ?></li>
+                <li><label>Дата визита</label>: <?= Html::encode($model->date) ?></li>
+                <li><label>Количество человек</label>: <?= Html::encode($model->amount_info) ?></li>
+                <li><label>Дополнительная информация</label>: <?= Html::encode($model->commentary) ?></li>
+            </ul>
+            <?= Html::a('На главную', ['/site/index'], ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+</div>
--- frontend/views/site/record.php
+++ frontend/views/site/record.php
@@ -6,24 +6,32 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Record */
 /* @var $form ActiveForm */
+
+$this->title = 'Запись';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="record">
 
-    <?php $form = ActiveForm::begin(); ?>
+<div class="site-about">
+    <div class="pl-5 pr-5 pt-2 pb-5 bg-light">
+    <div class="record w-50">
+
+        <?php $form = ActiveForm::begin(); ?>
 
 
-        <?= $form->field($model, 'name') ?>
+        <?= $form->field($model, 'name')->textInput(['autofocus' => true])  ?>
         <?= $form->field($model, 'surname') ?>
         <?= $form->field($model, 'patronymic') ?>
         <?= $form->field($model, 'phone') ?>
         <?= $form->field($model, 'email') ?>
         <?= $form->field($model, 'date') ?>
         <?= $form->field($model, 'amount_info') ?>
-        <?= $form->field($model, 'commentary') ?>
-    
+        <?= $form->field($model, 'commentary')->textarea(['rows' => 6]) ?>
+
         <div class="form-group">
             <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
         </div>
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
 
-</div><!-- record -->
+    </div>
+    </div>
+</div>
